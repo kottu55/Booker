3f26c9c35f394665a7ce52b52b6f1be06f885dbe
kottu55 <kotsuboon@gmail.com>
2019-10-08 12:46:56 +0000

    [Start] 4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+	root to: 'books#top'
+	get 'books/index' => 'books#index',as: "books"
+	get 'books/:id' => 'books#show',as:'book'
+	get 'books/:id/edit' => 'books#edit',as:'edit_book'
+	post 'books/index' => 'books#create'
+	patch 'books/:id' => 'books#update',as: 'update_book'
+	delete 'books/:id' => 'bookds#destroy',as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
